2efa9122015a90e4ab24241b23766a53867a5956
Parth D. Patel <parthpatel@Parths-MacBook-Air.local>
2023-04-05 23:49:56 +0530

    made oneone onetwo

---
+++ oneone.java
@@ -0,0 +1,5 @@
+class oneone{
+	public static void main(String a[]){
+		System.out.println("Welcome to Java");
+	}
+}
---
+++ onetwo.java
@@ -0,0 +1,14 @@
+import java.text.DecimalFormat;
+
+class onetwo{    
+    public static void main(String arg[]) {
+        double a=3.4, b=50.2, e=44.5, c=2.1, d=0.55, f=5.9;
+        
+        double x=(e*d - b*f)/(a*d - b*c), y=(a*f - e*c)/(a*d - b*c);
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println(df.format(x)+" "+df.format(y));
+    }
+}
+/*  taken reference about making #.## decimal as output from link
+    https://stackoverflow.com/questions/2538787/how-to-print-a-float-with-2-decimal-places-in-java
+ */
