bad2f1478c57c41cbbb965bd5875c1ec08b5e190
parthpatel2374 <dcpatel577@gmail.com>
2023-04-08 14:15:38 +0530

    added twothree twofour

---
+++ two/four.java
@@ -0,0 +1,51 @@
+class four {
+    public static void main(String arg[]){
+        int op1 = Integer.parseInt(arg[0]);
+        int op2 = Integer.parseInt(arg[2]);
+
+        switch(arg[1]){
+            case "=":
+                if(op1==op2)
+                    System.out.println("Both values are same.");
+                else{
+                    System.out.println("Both values are different.");
+                }
+                break;
+            case ">":
+                if(op1>op2){
+                    System.out.println("true");
+                }
+                else{
+                    System.out.println("false");
+                }
+                break;
+            case "<":
+                if(op1<op2){
+                    System.out.println("true");
+                }
+                else{
+                    System.out.println("false");
+                }
+                break;
+            case "+":
+                System.out.println((op1+op2));
+                break;
+            case "-":
+                System.out.println((op1-op2));
+                break;
+            case "*":
+                System.out.println((op1*op2));
+                break;
+            case "/":
+                if(op2 == 0){
+                    System.out.println("Division not possible.");
+                }
+                else{   
+                    System.out.println(((double)op1/op2));
+                }
+                break;
+            default:
+                System.out.println("Enter valid operator.");
+        }
+    }    
+}
---
+++ two/three.java
@@ -0,0 +1,20 @@
+class three {
+    public static void main(String arg[]){
+        int arr[][] = new int[4][];
+        for(int i=1; i<5; i++){
+            arr[i-1] = new int[i];
+        }
+        int k=0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                arr[i][j] = k++;
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }    
+}
