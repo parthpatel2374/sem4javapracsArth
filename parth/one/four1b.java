92b6384e832f69290e1a4bda0e58986b198d9047
parthpatel2374 <dcpatel577@gmail.com>
2023-04-13 00:56:37 +0530

    four1b.java and its class

---
+++ parth/one/four1b.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class four1b {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any letter : ");
+        char let = sc.next().toLowerCase().charAt(0);
+        
+        if(let == 'a' || let == 'e' || let == 'i' || let == 'o' || let == 'u'){
+            System.out.println("Entered character is a vovel.");
+        }
+        else{
+            System.out.println("Entered character is a consonant.");
+
+        }
+        sc.close();
+    }    
+}
